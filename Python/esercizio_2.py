084aefcd9c448487e9122e9bc36a2363732182fd
tommysulko <90907980+tommysulko@users.noreply.github.com>
2021-10-28 11:09:42 +0200

    Esercizi su Python
    Esercizi svolti in classe e come compito

---
+++ Python/esercizio_2.py
@@ -0,0 +1,6 @@
+x = 3.1415 #float, con 3,1415 cambia e invece con 3:1415 diventa "dupla"
+a = float(input("scrivi un numero decimale: "))
+
+y = x*a
+#f-string
+print(f"{x} moltiplicato per {a} fa {y}")  # è possibile utillizare le lettere con accento e apostrofo
---
+++ Python/esercizio_3_casa.py
@@ -0,0 +1,4 @@
+prima, seconda = 4, 7
+print(f"prima vale {prima} e seconda vale {seconda}")
+prima, seconda = seconda, prima
+print(f"a vale {prima} e b vale {seconda}")
