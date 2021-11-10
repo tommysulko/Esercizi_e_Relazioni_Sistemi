291b1fef190a62a6c0abfbca99bfe730dd17140b
tommysulko <90907980+tommysulko@users.noreply.github.com>
2021-11-10 16:24:27 +0100

    Update esercizio_11_casa.py

--- Python/esercizio_11_casa.py
+++ Python/esercizio_11_casa.py
@@ -4,6 +4,6 @@ x2 = int(input("Inserisci la coordinata x del secondo punto:"))
 y2 = int(input("Inserisci la coordinata y del secondo punto:"))
 tupla = (x1,y1,x2,y2)
 
-distanzaeculidea = ((x2**2 - x1**2) + (y2**2 - y1**2))*(1/2)
+distanzaeculidea = ((tupla[2]**2 - (tupla[0]**2) + ((tupla[3]**2 - (tupla[1]**2))*(1/2)
 
 print(f"La distanza euclidea dei due punti è {distanzaeculidea} ")
