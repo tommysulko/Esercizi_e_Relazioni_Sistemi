a459e7023cb283a51fa9caa3feece87b6e3cfb6c
tommysulko <90907980+tommysulko@users.noreply.github.com>
2021-11-18 11:20:30 +0100

    Update esercizio_18_casa.py

--- Python/esercizio_18_casa.py
+++ Python/esercizio_18_casa.py
@@ -1,7 +1,7 @@
 def isPrimo(num):
     contatore,divisore = 0,2
     primo = False
-    while divisore<=(numero*(1/2)) and contatore == 0:          
+    while divisore<=(num*(1/2)) and contatore == 0:          
         if num%divisore == 0:
             contatore=+1
         divisore+=1
@@ -12,12 +12,18 @@ def isPrimo(num):
         primo = False
     return primo
 
-contatorePrimi = 0
+#contatorePrimi = 0 
 mille = 1000
 
-for numero in range(2,mille):
+
+primi = [k for k in range(2,1003) if isPrimo(k)]
+print(f"Lista dei numeri primi: {primi}")
+
+"""for numero in range(2,mille):
     isPrimo(numero)
 
     if isPrimo(numero) == True:
         contatorePrimi+=1
+        primi.append(numero)
 print(f"I numeri primi prima di 1000 sono {contatorePrimi}")
+print(f"La lista dei numeri primi: {primi}")"""
