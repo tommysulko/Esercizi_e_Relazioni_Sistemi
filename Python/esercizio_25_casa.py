c2cb1bf1fb3867ee40a60b3dde8b608956e09fc2
tommysulko <90907980+tommysulko@users.noreply.github.com>
2021-11-25 10:43:43 +0100

    Update esercizio_25_casa.py

--- Python/esercizio_25_casa.py
+++ Python/esercizio_25_casa.py
@@ -1,4 +1,9 @@
-pitagorica = [conta*numero for conta in range (11) for numero in range(11)]
+#pitagorica = [conta*numero for conta in range (11)] for numero in range(11)]
+
+lista = [[conta*numero for conta in range (11)] for numero in range(11)]
+print(f"Lista pitagorica: {lista}")
+
+"""
 indice1 = 0
 indice2 = 11
 
@@ -6,3 +11,6 @@ for _ in range(11):
     print(pitagorica[indice1:indice2])
     indice1 += 11
     indice2 += 11 
+"""
+
+
