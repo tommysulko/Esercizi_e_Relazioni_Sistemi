cc1b3c5be7ed80ca841bfb8b7d8416dbef99c4e2
tommysulko <90907980+tommysulko@users.noreply.github.com>
2021-11-25 10:32:53 +0100

    Update esercizio_24_casa.py

--- Python/esercizio_24_casa.py
+++ Python/esercizio_24_casa.py
@@ -1,3 +1,3 @@
 import math
-ista = [numero for numero in range(1,1000) if math.sqrt(numero)/1==0]
+lista = [numero for numero in range(1,1000) if math.sqrt(numero)/1==0]
 print(f"La lita con solo dispari {lista}")
