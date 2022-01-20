e838e801c69319986e6b8c3a4f47517e8155144b
tommysulko <90907980+tommysulko@users.noreply.github.com>
2022-01-20 10:33:38 +0100

    Update esercizio_43.py

--- Python/esercizio_43.py
+++ Python/esercizio_43.py
@@ -1,27 +1,9 @@
-from turtle import Turtle, Screen
-import random
+serie = []
+def fibonacci(ultimo,penultimo,limite):
+    num = ultimo + penultimo
+    if num < limite:
+        serie.append(num)
+        fibonacci(num, ultimo, limite)
 
-tartaruga = Turtle()
-sfondo = Screen()
-tartaruga.speed(8)
-
-def stella(countRepeat):
-    coordX = random.randint(-300,300)
-    coordY = random.randint(-300,300)
-    tartaruga.penup()
-    tartaruga.goto(coordX,coordY)
-    tartaruga.pendown()
-    count = 0
-    angle = 144
-    while count < 5:
-        tartaruga.forward(100)
-        tartaruga.right(angle)
-        count += 1
-    countRepeat += 1
-    if countRepeat >= 50:
-        return
-    else:
-        stella(countRepeat)
-countRepeat = 0
-stella(countRepeat)
-sfondo.exitonclick()
+limite = int(input("Inserisci il numero massimo oltre cui i valori della serie non vengono stampati: "))
+print(fibonacci(1,1,limite))
