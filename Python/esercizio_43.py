c4e23f1d10d4c6a4c54374a9ef70093f761c3c86
tommysulko <90907980+tommysulko@users.noreply.github.com>
2022-01-20 10:33:13 +0100

    Update esercizio_43.py

--- Python/esercizio_43.py
+++ Python/esercizio_43.py
@@ -3,30 +3,25 @@ import random
 
 tartaruga = Turtle()
 sfondo = Screen()
-tartaruga.speed(5)
+tartaruga.speed(8)
 
-def main():
-    def stella(countRepeat):
-        coordX = random.randint(-250,250)
-        coordY = random.randint(-250,250)
-        tartaruga.penup()
-        tartaruga.goto(coordX,coordY)
-        tartaruga.pendown()
-        count = 0
-        angle = 144
-        while count <= 5:
-            tartaruga.forward(100)
-            tartaruga.right(angle)
-            count += 1
-        countRepeat += 1
-        if countRepeat >= 50:
-            return
-        else:
-            stella(countRepeat)
-            
-    countRepeat = 0
-    stella(countRepeat)
-    sfondo.exitonclick()
-
-if __name__ == "__main__":
-    main()
+def stella(countRepeat):
+    coordX = random.randint(-300,300)
+    coordY = random.randint(-300,300)
+    tartaruga.penup()
+    tartaruga.goto(coordX,coordY)
+    tartaruga.pendown()
+    count = 0
+    angle = 144
+    while count < 5:
+        tartaruga.forward(100)
+        tartaruga.right(angle)
+        count += 1
+    countRepeat += 1
+    if countRepeat >= 50:
+        return
+    else:
+        stella(countRepeat)
+countRepeat = 0
+stella(countRepeat)
+sfondo.exitonclick()
