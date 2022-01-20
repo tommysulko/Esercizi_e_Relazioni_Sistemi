ea3700bfdd3f9737b8b1521c1550bac7d1677f73
tommysulko <90907980+tommysulko@users.noreply.github.com>
2022-01-20 11:02:55 +0100

    Esercizio Python Stelle con classe

--- Python/esercizio_43.py
+++ Python/esercizio_43.py
@@ -1,9 +1,32 @@
-serie = []
-def fibonacci(ultimo,penultimo,limite):
-    num = ultimo + penultimo
-    if num < limite:
-        serie.append(num)
-        fibonacci(num, ultimo, limite)
+from turtle import Turtle, Screen
+import random
 
-limite = int(input("Inserisci il numero massimo oltre cui i valori della serie non vengono stampati: "))
-print(fibonacci(1,1,limite))
+tartaruga = Turtle()
+sfondo = Screen()
+tartaruga.speed(30)
+
+class stella():
+    def __init__(self,coordX,coordY,dim):
+        self.coordX = coordX
+        self.coordY = coordY
+        self.dim = dim
+    
+    def draw(self):
+        tartaruga.penup()
+        tartaruga.goto(self.coordX,self.coordY)
+        tartaruga.pendown()
+        count = 0
+        angle = 144
+        while count < 5:
+            tartaruga.forward(self.dim)
+            tartaruga.right(angle)
+            count += 1
+        #countRepeat += 1
+        #if countRepeat >= 50:
+            #return
+        #else:
+            #stella(countRepeat)
+
+for _ in range(50):
+    stellax = stella(random.randint(-280,280),random.randint(-280,280),random.randint(10,30))
+    stellax.draw()
